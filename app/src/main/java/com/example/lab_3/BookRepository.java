ed40a2ff64242adf13c338059ab73e0ef08b025d
Смаль Софія КП-41 <busik865@gmail.com>
2026-05-04 18:39:15 +0300

    BookRepository added.

---
+++ app/src/main/java/com/example/lab_3/BookRepository.java
@@ -0,0 +1,123 @@
+package com.example.lab_3;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+public class BookRepository {
+    private final DatabaseHelper dbHelper;
+
+    public BookRepository(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    public ArrayList<Book> getAllBooks() {
+        ArrayList<Book> books = new ArrayList<>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM books ORDER BY id DESC", null);
+
+        while (cursor.moveToNext()) {
+            Book book = new Book(
+                    cursor.getInt(cursor.getColumnIndexOrThrow("id")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("title")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("genre")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("author")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("createdAt"))
+            );
+            books.add(book);
+        }
+
+        cursor.close();
+        db.close();
+
+        return books;
+    }
+
+    public Book getBookById(int id) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(
+                "SELECT * FROM books WHERE id = ?",
+                new String[]{String.valueOf(id)}
+        );
+
+        Book book = null;
+
+        if (cursor.moveToFirst()) {
+            book = new Book(
+                    cursor.getInt(cursor.getColumnIndexOrThrow("id")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("title")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("genre")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("author")),
+                    cursor.getString(cursor.getColumnIndexOrThrow("createdAt"))
+            );
+        }
+
+        cursor.close();
+        db.close();
+
+        return book;
+    }
+
+    public void insertBook(Book book) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("title", book.getTitle());
+        values.put("genre", book.getGenre());
+        values.put("author", book.getAuthor());
+
+        db.insert("books", null, values);
+        db.close();
+    }
+
+    public void updateBook(Book book) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("title", book.getTitle());
+        values.put("genre", book.getGenre());
+        values.put("author", book.getAuthor());
+
+        db.update(
+                "books",
+                values,
+                "id = ?",
+                new String[]{String.valueOf(book.getId())}
+        );
+
+        db.close();
+    }
+
+    public void deleteBook(int id) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        db.delete(
+                "books",
+                "id = ?",
+                new String[]{String.valueOf(id)}
+        );
+
+        db.close();
+    }
+
+    public boolean checkSameTitleAndAuthorExists(String title, String author, int currentBookId) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(
+                "SELECT * FROM books WHERE title = ? AND author = ? AND id != ?",
+                new String[]{title, author, String.valueOf(currentBookId)}
+        );
+
+        boolean exists = cursor.moveToFirst();
+
+        cursor.close();
+        db.close();
+
+        return exists;
+    }
+}
