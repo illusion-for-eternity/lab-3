d5522a8f2795176124e21e96881aa27eef3458a8
Смаль Софія КП-41 <busik865@gmail.com>
2026-05-05 19:12:42 +0300

    Sorting UI logic implemented.

--- app/src/main/java/com/example/lab_3/MainActivity.java
+++ app/src/main/java/com/example/lab_3/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.lab_3;
 
 
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,9 +25,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RadioButton rb1 = findViewById(R.id.RadioButtonAuthor);
+        RadioButton rb2 = findViewById(R.id.RadioButtonTitle);
+        RadioButton rb3 = findViewById(R.id.RadioButtonGenre);
+
+        rb1.setOnClickListener(v -> loadBooks(repository.sortBy("author")));
+
+        rb2.setOnClickListener(v -> loadBooks(repository.sortBy("title")));
+
+        rb3.setOnClickListener(v -> loadBooks(repository.sortBy("genre")));
+
+        rb1.setVisibility(View.GONE);
+        rb2.setVisibility(View.GONE);
+        rb3.setVisibility(View.GONE);
+
         repository = new BookRepository(this);
         booksListView = findViewById(R.id.booksListView);
         Button addBookButton = findViewById(R.id.addBookButton);
+
+        Button sortButton = findViewById(R.id.sortButton);
+        sortButton.setOnClickListener(v -> {
+            rb1.setVisibility(View.VISIBLE);
+            rb2.setVisibility(View.VISIBLE);
+            rb3.setVisibility(View.VISIBLE);
+        });
 
         addBookButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, BookActivity.class);
@@ -45,21 +67,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        loadBooks();
+
+        RadioButton rb1 = findViewById(R.id.RadioButtonAuthor);
+        RadioButton rb2 = findViewById(R.id.RadioButtonTitle);
+        RadioButton rb3 = findViewById(R.id.RadioButtonGenre);
+
+        rb1.setVisibility(View.GONE);
+        rb2.setVisibility(View.GONE);
+        rb3.setVisibility(View.GONE);
+
+        loadBooks(repository.getAllBooks());
     }
 
-    private void loadBooks() {
+    private void loadBooks(ArrayList<Book> loadedBooks) {
         try {
-            books = repository.getAllBooks();
+            books = loadedBooks;
 
             ArrayList<String> bookTitles = new ArrayList<>();
 
-
             for (Book book : books) {
                 String createdAt = book.getCreatedAt().substring(0, 16);
-                bookTitles.add(    book.getTitle() + "\n" +book.getAuthor()
-                        + " | " + book.getGenre()
-                        + "\n" + "Created: " + createdAt);
+
+                bookTitles.add(book.getTitle() + "\n" +
+                        book.getAuthor() + " | " + book.getGenre() +
+                        "\nCreated: " + createdAt);
             }
 
             adapter = new ArrayAdapter<>(
@@ -69,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
             );
 
             booksListView.setAdapter(adapter);
+
         } catch (Exception e) {
             Toast.makeText(this, "There is problem in loading books", Toast.LENGTH_SHORT).show();
         }
